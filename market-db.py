890014b193781c4ec004f82a49f07256ac00ec4f
Ömercan Taşdemir <omercantasdemir@hotmail.com>
2020-09-28 11:38:50 +0300

    Add files via upload
    This is the first-ever storing system I created on python

---
+++ market-db.py
@@ -0,0 +1,28 @@
+def ekleme():    
+    global urun
+    global ID
+    ID=int(input("Ürün ID giriniz: "))
+    AD=str(input("Ürünün adını giriniz: "))
+    FIYAT=int(input("Ürün fiyatı giriniz: "))
+    urun={ID:{"ad":AD,"fiyat":FIYAT },}
+def okuma():
+    ara=int(input("Hangi ID aransın: "))
+    dsyoku=open("db.txt","r")
+    kont=dsyoku.readlines()
+    for i in kont:
+        if i.startswith(str(ara)):
+            print(i)
+def yazma():
+    dsy=open("db.txt","a")
+    dsy.write(str(ID).ljust(10)+" "+str(urun[ID]["ad"]).center(10)+" "+str(urun[ID]["fiyat"]).rjust(10)+"\n")
+
+secim=0
+while(secim!=3):
+    secim =int(input("Yapmak istediğiniz eylemi seçiniz: \n1) Ürün ekleme \n2)Ürün listeleme \n3)Çıkış\n"))
+    if secim==1:
+        ekleme()
+        yazma()
+        secim=0
+    elif secim==2:
+        okuma()
+        secim=0
