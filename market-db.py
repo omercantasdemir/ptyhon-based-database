e9b814a4419f6139555e2ec4610284a4c7ee9dde
Ömercan Taşdemir <omercantasdemir@hotmail.com>
2020-10-12 00:02:50 +0300

    update
    I am trying to use a few new techniques and it's not done yet. It's developing yet.

--- market-db.py
+++ market-db.py
@@ -9,13 +9,20 @@ def okuma():
     ara=int(input("Hangi ID aransın: "))
     dsyoku=open("db.txt","r")
     kont=dsyoku.readlines()
-    for i in kont:
-        if i.startswith(str(ara)):
-            print(i)
+    # for i in kont:
+    #     if i.startswith(str(ara)):
+    #         print(i)
+    
+    print(lambda x:x in kont if (x.startswith(str(ara))))
 def yazma():
-    dsy=open("db.txt","a")
+    while True:
+        try:
+            dsy=open("db.txt","a")
+        except:
+            dsy=open("db.txt","w")
+        else:
+            break
     dsy.write(str(ID).ljust(10)+" "+str(urun[ID]["ad"]).center(10)+" "+str(urun[ID]["fiyat"]).rjust(10)+"\n")
-
 secim=0
 while(secim!=3):
     secim =int(input("Yapmak istediğiniz eylemi seçiniz: \n1) Ürün ekleme \n2)Ürün listeleme \n3)Çıkış\n"))
